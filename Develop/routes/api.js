f4c8ba3a08c732ef0702e51c81a2d617123915c6
Edenilton <edeniltonborba.adm@gmail.com>
2020-02-18 10:46:52 +0100

    add route/get

--- Develop/routes/api.js
+++ Develop/routes/api.js
@@ -33,6 +33,14 @@ router.get("/api/workouts", (req, res) => {
 });
 
 router.get("/exercise?", (req, res) => {
+    res.sendFile(path.join(__dirname, "../public/exercise.html"));
+});
+
+router.get("/stats", (req, res) => {
+    res.sendFile(path.join(__dirname, "../public/stats.html"));
+});
+
+router.get("/exercise?", (req, res) => {
     res.sendFile(path.join(__dirname, "../public/stats.html"));
 });
 
