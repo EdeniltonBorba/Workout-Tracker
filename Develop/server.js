0de8b169e08fa4a644c5be5779826f3048535b76
Edenilton <edeniltonborba.adm@gmail.com>
2020-02-18 12:30:29 +0100

    add server.js

--- Develop/server.js
+++ Develop/server.js
@@ -6,4 +6,18 @@ const PORT = process.env.PORT || 3000;
 const app = express();
 
 app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
 app.use(express.static("public"));
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
+    useNewUrlParser: true,
+    useFindAndModify: false
+});
+
+app.use(require("./routes/api.js"));
+
+app.listen(PORT, () => {
+    Console.log(`App running on port ${PORT}!`);
+});
+
